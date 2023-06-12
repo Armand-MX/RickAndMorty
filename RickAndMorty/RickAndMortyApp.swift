f34e8780c444d19bf45775c7a900bfbe2a0a40d4
Armand-MX <123115197+Armand-MX@users.noreply.github.com>
2023-06-12 15:24:37 -0600

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Armando Alvarado on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
